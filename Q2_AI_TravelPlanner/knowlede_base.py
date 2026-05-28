f98d66179d76651233f9a2c7784ca88c5007fa2e
PolavarapuGirish <polavarapugirish85@gmail.com>
2026-05-28 17:25:52 +0530

    Create knowlede_base.py

---
+++ Q2_AI_TravelPlanner/knowlede_base.py
@@ -0,0 +1,69 @@
+tourist_places = {
+    "Goa": {
+        "type": "Beach",
+        "places": [
+            "Baga Beach",
+            "Calangute Beach",
+            "Fort Aguada"
+        ]
+    },
+
+    "Hyderabad": {
+        "type": "Historical",
+        "places": [
+            "Charminar",
+            "Golconda Fort",
+            "Ramoji Film City"
+        ]
+    },
+
+    "Delhi": {
+        "type": "Historical",
+        "places": [
+            "India Gate",
+            "Red Fort",
+            "Qutub Minar"
+        ]
+    },
+
+    "Manali": {
+        "type": "Hill Station",
+        "places": [
+            "Solang Valley",
+            "Rohtang Pass",
+            "Mall Road"
+        ]
+    }
+}
+
+food_data = {
+    "Goa": [
+        "Seafood",
+        "Fish Curry",
+        "Prawn Fry"
+    ],
+
+    "Hyderabad": [
+        "Biryani",
+        "Haleem",
+        "Kebab"
+    ],
+
+    "Delhi": [
+        "Chaat",
+        "Paratha",
+        "Butter Chicken"
+    ],
+
+    "Manali": [
+        "Momos",
+        "Trout Fish",
+        "Thukpa"
+    ]
+}
+
+budget_data = {
+    "Low": 5000,
+    "Medium": 15000,
+    "High": 40000
+}
